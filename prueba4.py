98b81435f5013630762c8185628b22fb86900423
Baryonic <43332834+Baryonic@users.noreply.github.com>
2024-02-25 11:35:57 +0100

    Add files via upload

---
+++ prueba4.py
@@ -0,0 +1,40 @@
+"""
+sun=True
+booleanaaaaa=True
+if(sun==booleanaaaaa):
+    print("sun")
+else:
+    print("no sun")
+if(sun):
+    print("sun es true")
+else:
+    print("sun es false")  
+if(sun==False):
+    print("rain")
+else:
+    print("no rain")
+if(sun!=booleanaaaaa):
+    print("sun es el conteario de booñleana")
+else:
+    print("sun == booleada")
+"""
+
+
+
+dia = int(input("dime q dia de numero es hoy"))
+
+
+if(dia==1):
+    print("hoy es lunes")
+elif(dia==2):
+    print("hoy es martes")
+elif(dia!=1 & dia!=2):
+    print("hoy no es ni lunes ni martes ni miercoles")
+
+
+
+
+elif(dia==3):
+    print("hoy es miercoles")
+else:
+    print("esto no se va a imprimir nunca")
